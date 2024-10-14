58691fc2105b76aa18ec2c39fb0a6497b681d34c
Kylon-Rose <kylonrose12345@gmail.com>
2024-10-14 19:53:41 -0400

    created scanner for user input

--- Diamond.java
+++ Diamond.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Diamond {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Please enter a number.");
+        int input = scanner.nextInt();
 }
 
 
