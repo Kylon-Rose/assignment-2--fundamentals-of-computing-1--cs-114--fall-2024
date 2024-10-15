bc0d4db009a5dd84cc44d852a541f2ca78e2257a
Kylon-Rose <kylonrose12345@gmail.com>
2024-10-14 20:34:48 -0400

    removed class notes from assignment

--- Diamond.java
+++ Diamond.java
@@ -15,35 +15,3 @@ public class Diamond {
 
  //Input//
 
-    //Odd (Every odd diamond starts with a single *). The amount of space you get until you get to the middle.//
-    //is 1 less than the previous line & starts to decrease again after the middle until 1.//
-    //3/2= 1 (with a remainder of 1 which is removed example) or 7/2=3 (with a remainder of 1 which is removed).//
-
-    //Print Output//
-
-    //Even//
-
-    //Print Output//
-
-    //Test//
-
-      //Sequence//
-        //Single Selection//
-         //(If) (example: If raining outside = take umbrella) (example: If not raining outside = ignore all together)
-          //True or False Statement, If you need more than 1 statement subordinate to the if statement use
-          // {}, example:
-            //if (raining outside){
-              // take umbrella;
-              // take raincoat;
-              //}
-    //A%B = A for all A<B  % is called modulo or mod.
-    //Repetition:
-      //For loop: for(int x = 0; x < 3; x++)
-      //                System.out.println("x");
-      //While loop:
-      //Do... while:
-    // Keep count of spaces, and asterisks
-    // i/2 = # of spaces on the first line
-    // If you hold the variable (i/2)-1 (applies until the middle)
-    // If you hold the variable (i/2)+1 (applies after the middle)
-    // For the even case it's top and bottom line is treated differently than the rest.
